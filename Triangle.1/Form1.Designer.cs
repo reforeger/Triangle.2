6f610730defa24d6e57ad0a7f0d434692dc7ea64
ADMIN <daniel.mihol@gmail.com>
2020-06-25 22:41:56 +0300

    Добавьте файлы проекта.

---
+++ Triangle.1/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Triangle._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.BorderSize = 10;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(424, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(287, 166);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "запуск";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Работа с треугольником";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Triangle.1/Triangle.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle._1
+{
+    class Triangle
+    {
+        public double a; //Первая сторона
+        public double b; //Вторая сторона
+        public double c; //Третия сторона
+        public double l; //Вторая сторона
+
+
+        // Свойства
+        public double GetSetA // получить или узнать значения стороны треугольника
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public double GetSetB
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public double GetSetC
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public double GetSetH
+        {
+            get { return l; }
+            set { l = value; }
+        }
+
+        public bool ExistTriangle // существует ли треугольник
+        {
+            get
+            {
+                if ((a < b + c) && (b < a + c) && (c < a + b))
+                    return true;
+                else return false;
+            }
+        }
+
+        public string TriangleType
+        {
+            get
+            {
+                if (a == b && b == c && c == a)
+                {
+                    return "Равносторонний";
+                }
+                else if (b == c || a == b || c == a)
+                {
+                    return "Равнобедренный";
+                }
+                else return "Разносторонний";
+            }
+        }
+
+
+        public Triangle(double A, double B, double C) // Конструктор
+        {
+            //Создаём с заданными длиннами сторон согласно заданию
+            a = A;
+            b = B;
+            c = C;
+            l = Height();
+        }
+
+        public Triangle(bool byHeight, double A, double L) // конструктор с линиями по краям
+        {
+            if (byHeight == true)
+            {
+                a = A;
+                l = L;
+                b = l;
+                c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(l, 2));
+            }
+            else
+            {
+                a = A;
+                l = L;
+                b = Math.Sqrt(Math.Pow(a / 2, 2) + Math.Pow(l, 2));
+                c = b;
+            }
+        }
+
+        public Triangle() // конструктор без атрибутов
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+        }
+
+        // Методы
+        public string OutputA() // вывод сторон
+        {
+            return Convert.ToString(a);
+        }
+
+        public string OutputB()
+        {
+            return Convert.ToString(b);
+        }
+
+        public string OutputC()
+        {
+            return Convert.ToString(c);
+        }
+
+        public double Perimeter() // периметр
+        {
+            double p = a + b + c;
+            return p;
+        }
+
+        public double HalfPerimeter() // полупериметр
+        {
+            return Perimeter() / 2;
+        }
+
+        public double Surface() // площадь
+        {
+            double p = HalfPerimeter();
+            double s = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+            return s;
+        }
+
+        public double Height() // высота
+        {
+            double p = HalfPerimeter();
+            double h = 2 * Math.Sqrt(p * (p - a) * (p - b) * (p - c)) / a;
+            return h;
+        }
+    }
+}
